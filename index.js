900f18c12ca35642e357c502dc1463e7c5413255
bryce <50299051+bryce-seifert@users.noreply.github.com>
2020-08-31 20:02:23 -0500

    Replaced "destroy" references
    –Replaced references to destroy with obs.disconnect

--- index.js
+++ index.js
@@ -19,13 +19,13 @@ instance.prototype.updateConfig = function(config) {
 	var self = this;
 	self.config = config;
 	self.log('debug','updateConfig() destroying and reiniting..');
-	self.destroy();
+	self.obs.disconnect();
 	self.init();
 };
 
 instance.prototype.init = function() {
 	var self = this;
-
+	self.disable = false;
 	self.status(self.STATUS_WARN, "Connecting");
 	if (self.obs !== undefined) {
 		self.obs.disconnect();
@@ -76,12 +76,10 @@ instance.prototype.init = function() {
 		});
 
 		self.obs.on('ConnectionClosed', function() {
-			if (self.disable != true) {
+			if (self.disable != true) {	
 				self.log('error','Connection lost to OBS.');
 				self.status(self.STATUS_ERROR);
-				self.destroy();
 				self.init();
-				self.log('debug', 'obs.on.connecitonclosed')
 			} else {
 				
 			}
@@ -312,7 +310,6 @@ instance.prototype.updateInfo = function() {
 // When module gets deleted
 instance.prototype.destroy = function() {
 	var self = this;
-	self.disable = true;
 	self.scenes = [];
 	self.transitions = [];
 	self.states = {};
@@ -325,7 +322,7 @@ instance.prototype.destroy = function() {
 	if (self.tcp !== undefined) {
 		self.tcp.destroy();
 	}
-	debug('destroy', self.id);
+	self.disable = true;
 };
 
 instance.prototype.actions = function() {
@@ -506,7 +503,7 @@ instance.prototype.action = function(action) {
 
 	if (action.action == 'reconnect') {
 		self.log('debug','reconnecting, destroying and reiniting..');
-		self.destroy();
+		self.obs.disconnect();
 		self.init();
 		return;
 	}
@@ -582,7 +579,7 @@ instance.prototype.action = function(action) {
 	handle.catch(error => {
 		if (error.code == "NOT_CONNECTED") {
 			self.log('warn', 'Send to OBS failed. Re-start OBS manually. Starting re-init');
-			self.destroy();
+			self.obs.disconnect();
 			self.init();
 		} else {
 			self.log('debug', error.error);
@@ -592,7 +589,6 @@ instance.prototype.action = function(action) {
 
 instance.prototype.init_feedbacks = function() {
 	var self = this;
-	self.disable = false;
 	// feedbacks
 	var feedbacks = {};
 	feedbacks['streaming'] = {
