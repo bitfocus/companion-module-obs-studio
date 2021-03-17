b8c7be4456dd492edb16f8a1b6ee13b4c8b1ba38
bryce <50299051+bryce-seifert@users.noreply.github.com>
2021-03-17 15:40:39 -0500

    Add default transition behavior

--- index.js
+++ index.js
@@ -612,7 +612,7 @@ instance.prototype.actions = function() {
 				{
 					type: 'dropdown',
 					label: 'Transition to use',
-					id: 'transition',
+					default: 'Default',
 					default: null,
 					choices: self.transitionlist,
 					required: false
@@ -844,12 +844,19 @@ instance.prototype.action = function(action) {
 		case 'do_transition':
 			var options = {};
 			if (action.options && action.options.transition) {
-				options['with-transition'] = {
-					name: action.options.transition
-				};
-
-				if (action.options.transition_time > 0) {
-					options['with-transition']['duration'] = action.options.transition_time;
+				if (action.options.transition == 'Default') {
+					options['with-transition'] = {
+					};
+					if (action.options.transition_time > 0) {
+						options['with-transition']['duration'] = action.options.transition_time;
+					}
+				} else {
+					options['with-transition'] = {
+						name: action.options.transition
+					};
+					if (action.options.transition_time > 0) {
+						options['with-transition']['duration'] = action.options.transition_time;
+					}
 				}
 			}
 
