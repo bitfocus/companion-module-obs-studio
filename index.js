7311bc559fc4fc2480ad378d9784cb0b0fb38d5e
bryce <50299051+bryce-seifert@users.noreply.github.com>
2023-03-22 21:51:33 -0500

    Feat: Studio Mode feedback (#206)

--- index.js
+++ index.js
@@ -452,6 +452,7 @@ class OBSInstance extends InstanceBase {
 		//UI
 		this.obs.on('StudioModeStateChanged', (data) => {
 			this.states.studioMode = data.studioModeEnabled ? true : false
+			this.checkFeedbacks('studioMode')
 		})
 	}
 
