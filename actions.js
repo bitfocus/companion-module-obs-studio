183954dc619e9732017b3c35d945ca1e6a68523e
bryce <50299051+bryce-seifert@users.noreply.github.com>
2023-07-04 13:51:40 -0500

    feat: adjust source volume by percentage

--- actions.js
+++ actions.js
@@ -512,7 +512,7 @@ export function getActions() {
 		},
 	}
 	actions['adjust_volume'] = {
-		name: 'Adjust Source Volume',
+		name: 'Adjust Source Volume (dB)',
 		description: 'Adjusts the volume of a source by a specific increment',
 		options: [
 			{
@@ -539,6 +539,69 @@ export function getActions() {
 			}
 
 			this.sendRequest('SetInputVolume', { inputName: action.options.source, inputVolumeDb: newVolume })
+		},
+	}
+	actions['adjust_volume_percent'] = {
+		name: 'Adjust Source Volume (Percentage)',
+		description: 'Adjusts the volume of a source based on a percentage of the OBS volume slider',
+		options: [
+			{
+				type: 'dropdown',
+				label: 'Source',
+				id: 'source',
+				default: this.audioSourceListDefault,
+				choices: this.audioSourceList,
+			},
+			{
+				type: 'number',
+				label: 'Percent Adjustment',
+				id: 'percent',
+				default: 0,
+				min: -100,
+				max: 100,
+				range: true,
+			},
+		],
+		callback: (action) => {
+			//Standard offset values (aka how the OBS code determines slider percentage)
+			let LOG_RANGE_DB = 96.0
+			let LOG_OFFSET_DB = 6.0
+			let LOG_OFFSET_VAL = -0.77815125038364363
+			let LOG_RANGE_VAL = -2.00860017176191756
+
+			//Calculate current "percent" of volume slider in OBS
+			let dB = this.sources[action.options.source].inputVolume
+			let currentPercent = 0.0
+			if (dB >= 0.0) {
+				currentPercent = 100.0
+			} else if (dB <= -96.0) {
+				currentPercent = 0.0
+			} else {
+				currentPercent = ((-Math.log10(-dB + 6.0) - LOG_RANGE_VAL) / (LOG_OFFSET_VAL - LOG_RANGE_VAL)) * 100.0
+			}
+
+			//Calculate new "percent" of volume slider
+			let percentAdjustment = Math.abs(action.options.percent)
+
+			let newPercent
+			if (action.options.percent > 0) {
+				newPercent = currentPercent + percentAdjustment
+			} else {
+				newPercent = currentPercent - percentAdjustment
+			}
+			newPercent = newPercent / 100
+			let newDb
+			if (newPercent >= 1.0) {
+				newDb = 0.0
+			} else if (newPercent <= 0.0) {
+				newDb = -100.0
+			} else {
+				newDb =
+					-(LOG_RANGE_DB + LOG_OFFSET_DB) * Math.pow((LOG_RANGE_DB + LOG_OFFSET_DB) / LOG_OFFSET_DB, -newPercent) +
+					LOG_OFFSET_DB
+			}
+
+			this.sendRequest('SetInputVolume', { inputName: action.options.source, inputVolumeDb: newDb })
 		},
 	}
 	actions['setSyncOffset'] = {
